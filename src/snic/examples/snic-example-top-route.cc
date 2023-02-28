988677df9a934803aa9cf8a5fc6ff8ac8f669dda
William Lin <wlsaidhi@gmail.com>
2023-02-27 17:13:12 -0800

    wip

--- src/snic/examples/snic-example-top-route.cc
+++ src/snic/examples/snic-example-top-route.cc
@@ -104,22 +104,27 @@ main(int argc, char* argv[])
 
     NS_LOG_INFO("Creating second sNIC cluster.");
     CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
-    NS_LOG_INFO("Creating 3rd sNIC cluster.");
-    CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
-    // NS_LOG_INFO("Creating 4th sNIC cluster.");
-    CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
+    // NS_LOG_INFO("Creating 3rd sNIC cluster.");
+    //  CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
+    //// NS_LOG_INFO("Creating 4th sNIC cluster.");
+    // CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
 
-    CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
-    CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
-    // connect two snics together
-    NetDeviceContainer snicLink = csmaHelper.Install(csmaSwitches);
+    // CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
+    // CreateSnic(snics, 1, terminals, csmaSwitches, terminalDevices, csmaHelper, snicHelper);
+    //  connect two snics together
+    NetDeviceContainer snicLink;
+    for (NodeContainer::Iterator i = csmaSwitches.Begin(); i != csmaSwitches.End(); ++i)
+    {
+        snicLink.Add(csmaHelper.Install(*i));
+    }
+    //= csmaHelper.Install(csmaSwitches);
     // swtch.ConnectTwoSnic(snic1, snic2);
     // swtch.Install(switchNode, snicLink.Get(0));
     // swtch2.Install(switchNode2, snicLink.Get(1));
-    snicHelper.AddPort(snics.Get(0), snicLink.Get(0));
-    snicHelper.AddPort(snics.Get(1), snicLink.Get(1));
-    snicHelper.AddPort(snics.Get(2), snicLink.Get(2));
-    //  snicHelper.AddPort(snics.Get(3), snicLink.Get(3));
+    snicHelper.AddPort(snics.Get(0), snicLink.Get(1));
+    snicHelper.AddPort(snics.Get(1), snicLink.Get(0));
+    // snicHelper.AddPort(snics.Get(2), snicLink.Get(2));
+    //   snicHelper.AddPort(snics.Get(3), snicLink.Get(3));
 
     // Add internet stack to the terminals
     SnicStackHelper internet;
